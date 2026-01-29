3db4228260fd273c7d81e39aac66b4f87be27017
arwa123-dridi <arwa.dridi@esprit.tn>
2026-01-29 19:21:13 +0100

    ajout

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+
 class DepartmentControllerTest {
 
     @Mock
